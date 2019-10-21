f85a0bdadaf127c7557ca16bfaac68bad0618e1e
julioenred <julioperezdecastro@gmail.com>
2019-10-21 02:15:12 +0200

    get strikes and fires

--- index.php
+++ index.php
@@ -20,6 +20,59 @@ foreach ($digits_of_number as $key => $digit)
     $number = $number . $digit;
 }
 
-var_dump($number); exit;
+$number_player = $number;
+
+$digits_of_number = [];
+
+for ($i = 0; $i < NUMBER_OF_DIGITS; $i++) 
+{ 
+    do 
+    {
+        $new_digit = rand(0, 9);
+    } while (in_array($new_digit, $digits_of_number));
+
+    $digits_of_number[] = $new_digit;
+}
+
+$number = '';
+foreach ($digits_of_number as $key => $digit)
+{
+    $number = $number . $digit;
+}
+
+$select_number_random_to_play = $number;
+
+$select_number_random_to_play_splitted = str_split($select_number_random_to_play);
+$number_player_splitted = str_split($number_player);
+
+$strikes = 0;
+$fires = 0;
+
+foreach ($number_player_splitted as $digit_number_key => $digit_number)
+{
+    if (in_array($digit_number, $select_number_random_to_play_splitted)) 
+    {
+        $select_number_key = array_search($digit_number, $select_number_random_to_play_splitted);
+        if ($digit_number_key == $select_number_key) 
+        {
+            $strikes = $strikes + 1;
+        }
+        else 
+        {
+            $fires = $fires + 1;
+        }
+    }
+}
+
+
+echo 'number_player: ' . $number_player . '<br>';
+echo 'select_number: ' . $select_number_random_to_play . '<br>';
+
+echo 'strikes: ' . $strikes . ' y fires: ' . $fires;
+
+
+
+
+
 
  
