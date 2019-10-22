3a472d78e8a2079facf745b9aab3864c787395e1
julioenred <julioperezdecastro@gmail.com>
2019-10-23 00:15:28 +0200

    create new atributes in class strike

--- strike.php
+++ strike.php
@@ -3,6 +3,54 @@
 class Strike
 {
     const NUMBER_OF_DIGITS = 4;
+    private $strikes;
+    private $fires;
+
+    public function __construct()
+    {
+        $this->strikes = 0;
+        $this->fires = 0;
+    }
+
+    public function run()
+    {
+        $number_player = $this->calculate_random_number();
+        $cont = 1;
+
+        do 
+        {
+            $this->reset_element('strikes');
+            $this->reset_element('fires');
+
+            $select_number_random_to_play = $this->calculate_random_number();
+
+            $select_number_random_to_play_splitted = str_split($select_number_random_to_play);
+            $number_player_splitted = str_split($number_player);
+
+            foreach ($number_player_splitted as $digit_number_key => $digit_number)
+            {
+                if (in_array($digit_number, $select_number_random_to_play_splitted)) 
+                {
+                    $select_number_key = array_search($digit_number, $select_number_random_to_play_splitted);
+                    if ($digit_number_key == $select_number_key) 
+                    {
+                        $this->strikes = $this->strikes + 1;
+                    }
+                    else 
+                    {
+                        $this->fires = $this->fires + 1;
+                    }
+                }
+            }
+
+            echo 'TRY: ' . $cont . '<br>';
+            echo 'number_player: ' . $number_player . '<br>';
+            echo 'select_number: ' . $select_number_random_to_play . '<br>';
+            echo 'strikes: ' . $this->strikes . ' y fires: ' . $this->fires;
+            echo '<br><br>';
+            $cont++;
+        } while ($this->strikes != 4);
+    }
 
     private function calculate_random_number()
     {
@@ -27,43 +75,8 @@ class Strike
         return $number;
     }
 
-    public function run()
+    private function reset_element($element)
     {
-        $number_player = $this->calculate_random_number();
-        $cont = 1;
-
-        do 
-        {
-            $select_number_random_to_play = $this->calculate_random_number();
-
-            $select_number_random_to_play_splitted = str_split($select_number_random_to_play);
-            $number_player_splitted = str_split($number_player);
-
-            $strikes = 0;
-            $fires = 0;
-
-            foreach ($number_player_splitted as $digit_number_key => $digit_number)
-            {
-                if (in_array($digit_number, $select_number_random_to_play_splitted)) 
-                {
-                    $select_number_key = array_search($digit_number, $select_number_random_to_play_splitted);
-                    if ($digit_number_key == $select_number_key) 
-                    {
-                        $strikes = $strikes + 1;
-                    }
-                    else 
-                    {
-                        $fires = $fires + 1;
-                    }
-                }
-            }
-
-            echo 'TRY: ' . $cont . '<br>';
-            echo 'number_player: ' . $number_player . '<br>';
-            echo 'select_number: ' . $select_number_random_to_play . '<br>';
-            echo 'strikes: ' . $strikes . ' y fires: ' . $fires;
-            echo '<br><br>';
-            $cont++;
-        } while ($strikes != 4);
+        $this->$element = 0;
     }
 }
